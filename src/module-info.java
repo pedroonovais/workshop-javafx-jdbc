eed28ee40fe461b34a4ebf8610bd48b4b29a4056
pedroonovais <phenrique101007@gmail.com>
2022-03-03 09:58:32 -0300

    DepatmentService

--- src/module-info.java
+++
@@ -1,9 +0,0 @@
-module javafx3 {
-	requires javafx.controls;
-	requires javafx.fxml;
-	requires java.sql;
-	
-	opens application to javafx.graphics, javafx.fxml;
-	
-	opens gui to javafx.graphics, javafx.fxml;
-}
